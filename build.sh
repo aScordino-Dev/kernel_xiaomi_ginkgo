d15abf9f849b1ca4c62d383f106ffb8bd1c17c17
Antonino Scordino <git@antonino.cf>
2022-07-03 11:52:22 +0000

    build.sh: Add support for AnyKernel
     - Add two three variables: 'ANYKERNEL', 'ANYKERNEL_DIR' & 'KDIR'
        * 'ANYKERNEL' is the repository that git will clone
        * 'ANYKERNEL_DIR' is the directory of AnyKernel
        * 'KDIR' is the kernel directory.
     - Ask the user to input kernel version before building
     - Delete arch/arm64/boot/Image before building
     - Add different variables used for the ZIP filename etc.
     - Show MD5 & Link for the ZIP file
     - If the ZIP file does not create, interrupt the AnyKernl process
     - Exit if the Image file does not exist.
    
    Thanks to [@]PeroSar1111 for the massive help with this bash script.
    
    Co-authored-by: PeroSar <perosar1111@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Variables
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 TOOLCHAIN="kdrag0n/proton-clang"
+AK3="aScordino/android_AnyKernel"
 TOOLCHAIN_DIR="/home/$USER/toolchain"
+AK3_DIR="/home/$USER/AnyKernel3"
+KDIR="$(pwd)"
 
 # Options
 if [[ ${1-} == "-c" || ${1-} == "--clean" ]]; then
@@ -11,13 +15,13 @@ if [[ ${1-} == "-c" || ${1-} == "--clean" ]]; then
     echo "[!] Cleaned output directory."
 fi
 
-if [[ $1 == "-r" || $1 == "--regen" ]]; then
+if [[ ${1-} == "-r" || ${1-} == "--regen" ]]; then
     cp out/.config arch/arm64/configs/${DEFCONFIG}
     echo -e "[!] Defconfig regenerated successfully."
     exit 0
 fi
 
-# Clone toolchain
+# Clone toolchain & AnyKernel3
 if [ -d "$TOOLCHAIN_DIR" ]; then
     echo "[!] Toolchain directory exists. Skipping..."
 else
@@ -25,14 +29,39 @@ else
     git clone https://github.com/"${TOOLCHAIN}" "${TOOLCHAIN_DIR}" --depth=1 >/dev/null 2>&1
 fi
 
+if [ -d "$AK3_DIR" ]; then
+    echo "[!] AnyKernel3 directory exists. Skipping..."
+else
+    echo "[…] Cloning ${AK3}"
+    git clone https://github.com/"${AK3}" "${AK3_DIR}" --depth=1 >/dev/null 2>&1
+fi
+
 # Building
 PATH="${TOOLCHAIN_DIR}/bin/:$PATH"
 export KBUILD_COMPILER_STRING="${TOOLCHAIN_DIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export KBUILD_BUILD_USER="antonino"
 export KBUILD_BUILD_HOST="scordino"
+read -rp "[?] Insert kernel version: " VERSION
+if [ -f out/arch/arm64/boot/Image ]; then rm -rf out/arch/arm64/boot/Image; fi
 
 echo "[…] Starting compilation..."
 make ${DEFCONFIG} >/dev/null 2>&1
 make -j$(nproc --all) \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CC=clang
+
+# Zipping
+NAME="rubik"
+CODENAME="ginkgo"
+ZIP="$NAME"-"$CODENAME"-"$VERSION".zip
+
+if [ -f out/arch/arm64/boot/Image ]; then
+    cp "out/arch/arm64/boot/Image" "${AK3_DIR}"
+    cd "${AK3_DIR}" || exit
+    zip -rq9 "${KDIR}/../${ZIP}" * -x "README.md"  || { echo -e "[✘] Failed to create ZIP file."; exit 0; }
+    cd "$KDIR" || exit
+    echo -n ""[i] Link:" $(curl --silent -T "${KDIR}/../$ZIP" temp.sh)"
+        printf "\n"
+    echo ""[i] MD5:" $(md5sum ../"${ZIP}" | cut -d' ' -f1)"
+    echo "[✓] Kernel successfully zipped."
+fi
